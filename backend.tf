afc3aec0b9ee1b6494cce79945761ec3b5cd4c67
sai-kumar9020 <100404258+sai-kumar9020@users.noreply.github.com>
2025-07-09 00:50:38 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 
 terraform {
   backend "s3" {
-    bucket       = "api-gateway-lambda"
-    key          = "api-gateway-lambda"
+    bucket       = "api-gateway-lambda-ses"
+    key          = "api-gateway-lambda-ses"
     region       = "us-west-1"
     encrypt      = true
     use_lockfile = true
