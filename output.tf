cbcc501ddd52160d791b6e07e6b97b05e369863e
sai-kumar9020 <100404258+sai-kumar9020@users.noreply.github.com>
2025-07-09 10:16:23 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,4 +1,5 @@
 output "api_gateway_url" {
-  value = "${aws_api_gateway_deployment.deployment.invoke_url}/${aws_api_gateway_stage.prod.stage_name}/contact"
+  value = "https://${aws_api_gateway_rest_api.contact_api.id}.execute-api.${var.aws_region}.amazonaws.com/${aws_api_gateway_stage.prod.stage_name}/contact"
+  description = "Invoke URL for the API Gateway endpoint"
 }
 
